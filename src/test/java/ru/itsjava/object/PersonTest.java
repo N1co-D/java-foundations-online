f83e6304a976aad2c3c92ce60400bfd56d3cace7
dmark <dmarkevich@gmail.com>
2023-01-08 18:21:11 +0300

    Add PersonTest (HW)

---
+++ src/test/java/ru/itsjava/object/PersonTest.java
@@ -0,0 +1,70 @@
+package ru.itsjava.object;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Класс Person должен: ")
+public class PersonTest {
+
+    public static final String NEW_NAME = "Александр";
+    public static final int NEW_AGE = 40;
+    public static final String DEFAULT_NAME = "Иван";
+    public static final int DEFAULT_AGE = 30;
+
+    @DisplayName(" корректно создаваться конструктором")
+    @Test
+    public void shouldHaveCorrectConstruction() {
+        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+        Assertions.assertEquals(DEFAULT_NAME, teacherOfGroup1.getName());
+        Assertions.assertEquals(DEFAULT_AGE, teacherOfGroup1.getAge());
+    }
+
+    @DisplayName(" корректно изменять параметр имени человека")
+    @Test
+    public void shouldHaveCorrectUpdatePersonName() {
+        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+        teacherOfGroup1.setName(NEW_NAME);
+        Assertions.assertEquals(NEW_NAME, teacherOfGroup1.getName());
+    }
+
+    @DisplayName(" корректно изменять параметр возраста человека")
+    @Test
+    public void shouldHaveCorrectUpdatePersonAge() {
+        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+        teacherOfGroup1.setAge(NEW_AGE);
+        Assertions.assertEquals(NEW_AGE, teacherOfGroup1.getAge());
+    }
+
+    @DisplayName(" корректно изменять возраст человека в день Рождения")
+    @Test
+    public void shouldHaveCorrectUpdatePersonAgeIfBirthsday() {
+        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+        teacherOfGroup1.birthsday();
+        Assertions.assertEquals(DEFAULT_AGE + 1, teacherOfGroup1.getAge());
+    }
+
+//    @DisplayName(" корректно проверять возраст человека перед покупкой пива")
+//    @Test
+//    public void shouldHaveCorrectCheckingPersonAgeForTakingBeer() {
+//        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+//        teacherOfGroup1.takeBeer();
+//        if (DEFAULT_AGE > 18) {
+//            Assertions.assertEquals(teacherOfGroup1.takeBeer(), true);
+//        } else {
+//            Assertions.assertEquals(teacherOfGroup1.takeBeer(), false);
+//        }
+//    }
+
+        @DisplayName(" корректно проверять возраст человека перед покупкой пива")
+    @Test
+    public void shouldHaveCorrectCheckingPersonAgeForTakingBeer() {
+        Person teacherOfGroup1 = new Person(DEFAULT_NAME, DEFAULT_AGE);
+        teacherOfGroup1.takeBeer();
+        if (DEFAULT_AGE > 18) {
+            Assertions.assertTrue(teacherOfGroup1.takeBeer());
+        } else {
+            Assertions.assertFalse(teacherOfGroup1.takeBeer());
+        }
+    }
+
+}
