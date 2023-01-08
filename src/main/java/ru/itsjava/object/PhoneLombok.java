a439e12705461be19ce1b5b7a32ba3c54ed991b6
dmark <dmarkevich@gmail.com>
2023-01-08 13:29:28 +0300

    Add "My Class2"

---
+++ src/main/java/ru/itsjava/object/PhoneLombok.java
@@ -0,0 +1,14 @@
+package ru.itsjava.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PhoneLombok {
+    private final String model;
+    private int storage;
+    private boolean modul5G;
+    private boolean nfc;
+
+}
