cf5bfbd8210c51054d870ff854e712e3886b7691
dmark <dmarkevich@gmail.com>
2023-01-08 18:19:07 +0300

    Add "My Class2"

---
+++ src/main/java/ru/itsjava/object/Person.java
@@ -0,0 +1,24 @@
+package ru.itsjava.object;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Person {
+    private String name;
+    private int age;
+
+    public void birthsday() {
+        age += 1;
+    }
+
+    public boolean takeBeer() {
+        if (age > 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
