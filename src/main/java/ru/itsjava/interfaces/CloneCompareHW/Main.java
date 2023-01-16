de30258fbc60f7ceb70e7e7227835fd3d12844be
dmark <dmarkevich@gmail.com>
2023-01-16 16:43:42 +0300

    HW Corrections (CompareTo)

--- src/main/java/ru/itsjava/interfaces/CloneCompareHW/Main.java
+++ src/main/java/ru/itsjava/interfaces/CloneCompareHW/Main.java
@@ -5,19 +5,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Person ivan = new Person("Иван", "Васильев", 17);
-        Person vasya = new Person("Степан", "Королев", 21);
-        System.out.println("Результат = " + ivan.compareTo(vasya));
+        Person stepan = new Person("Степан", "Грибов", 21);
+        System.out.println("Результат = " + ivan.compareTo(stepan));
 
         System.out.println("---------------------------------------------------------------------------");
 
         Person alex = new Person("Александр", "Милославский", 32);
-        Person vladimir = new Person("Владимир", "Грибов", 15);
+        Person vladimir = new Person("Владимир", "Милославский", 15);
         System.out.println("Результат = " + alex.compareTo(vladimir));
 
         System.out.println("---------------------------------------------------------------------------");
 
+        Person petr1 = new Person("Петр", "Воробьев", 32);
+        Person petr2 = new Person("Петр", "Воробьев", 25);
+        System.out.println("Результат = " + petr1.compareTo(petr2));
+
+        System.out.println("---------------------------------------------------------------------------");
+
         System.out.print("Массив до сортировки: ");
-        Person[] people = {ivan, vasya, alex, vladimir};
+        Person[] people = {ivan, stepan, alex, vladimir, petr1, petr2};
         System.out.println(Arrays.toString(people));
         Arrays.sort(people);
         System.out.print("Массив после сортировки: ");
--- src/main/java/ru/itsjava/interfaces/CloneCompareHW/Person.java
+++ src/main/java/ru/itsjava/interfaces/CloneCompareHW/Person.java
@@ -10,15 +10,16 @@ public class Person implements Comparable<Person>, Cloneable {
 
     @Override
     public int compareTo(Person person) {
-        int result = surname.compareTo(person.surname);
-        if (result == 0) {
-//            System.out.println("Фамилии одинаковы, следовательно сравниваем имена!");
-//            System.out.println("Если результат = 0, человек сравнивается сам с собой. Если результат меньше 0, значит имя первого человека лексикографически меньше имени второго человека. Если больше 0, то наоборот.");
-            return name.compareTo(person.name);
+        int surnameResult = surname.compareTo(person.surname);
+        int nameResult = name.compareTo(person.name);
+        if (surnameResult == 0) {
+            if (nameResult == 0) {
+                return (int) (age - person.age); // ФИ у людей одинаковые, производим сравнение по возрасту. Если возвращается 0, значит сравниваются одинаковые люди
+            } else {
+                return nameResult;
+            }
         } else {
-//            System.out.println("Фамилии разные, следовательно сравниваем разницу в возрасте!");
-//            System.out.println("Если результат = 0, люди ровесники. Если ответ больше 0, то первый человек старше на итоговый результат. Если ответ меньше 0, значит возраст второго человека выше возраста первого человека на результат в модуле.");
-            return (int) (age - person.age);
+            return surnameResult;
         }
     }
 
@@ -26,8 +27,9 @@ public class Person implements Comparable<Person>, Cloneable {
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Person {" + name + " " + surname + ", " + (int) age + "}";
     }
 
