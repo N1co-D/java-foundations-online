72c99884f3295ba406301164542261598d9b881c
dmark <dmarkevich@gmail.com>
2023-02-03 18:17:13 +0300

    ArrayList vs LinkedList (HW)

--- src/main/java/ru/itsjava/collections/lists/arrayList/MyArrayList.java
+++ src/main/java/ru/itsjava/collections/lists/arrayList/MyArrayList.java
@@ -3,25 +3,16 @@ package ru.itsjava.collections.lists.arrayList;
 public class MyArrayList {
     private int realSize;
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] array;                                 //why????
+    private Object[] array;                                 //?
 
     public MyArrayList() {
-        array = new Object[DEFAULT_CAPACITY];               //why????
+        array = new Object[DEFAULT_CAPACITY];               //?
         realSize = 0;
     }
 
     public int size() {
         return realSize;
     }
-
-//    public boolean isEmpty() {
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] != null) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
 
     public boolean isEmpty() {
         if (realSize == 0) {
@@ -40,19 +31,50 @@ public class MyArrayList {
         return false;
     }
 
-    public boolean add(Object o) {
-        if (realSize == array.length) {
+//    public boolean add(Object o) {         //?
+//        if (realSize == array.length) {
+//            Object[] resArray = new Object[array.length * 3 / 2 + 1];
+//            //копирование массива
+//            System.arraycopy(array, 0, resArray, 0, array.length);
+//            array = resArray;
+//        }
+//        array[realSize++] = o; //вставка нашего элемента
+//        return true;
+//    }
+
+    public boolean add(Object o) {         //?
+        if (realSize >= array.length) {
             Object[] resArray = new Object[array.length * 3 / 2 + 1];
             //копирование массива
-            System.arraycopy(array, 0, resArray, 0, array.length);
+            for (int i = 0; i < array.length; i++) {
+                resArray[i] = array[i];
+            }
             array = resArray;
+            //вставка нашего элемента
         }
-        array[realSize++] = o; //вставка нашего элемента
+        array[realSize] = o;
+        realSize++;
         return true;
     }
 
     public boolean remove(Object o) {
-        return false;
+        int delIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null && array[i].equals(o)) {
+                delIndex = i;
+                break;
+            }
+        }
+        for (int i = delIndex; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+
+        if (delIndex == -1) {
+            return false;
+        } else {
+            realSize--;
+            return true;
+        }
     }
 
     public void clear() {
@@ -61,29 +83,103 @@ public class MyArrayList {
         }
     }
 
-
     public Object get(int index) {
-        return null;
+        checkIndex(index);
+        Object resElement = array[index];
+        return resElement;
     }
 
     public Object set(int index, Object element) {
-        return null;
+        checkIndex(index);
+        array[index] = element;
+        return element;
     }
 
-    public void add(int index, Object element) {
+//    public void add (int index, Object element) {
+//        Object[] resArray = new Object[array.length + 1];
+//        for (int i = 0; i < index; i++) {
+//            resArray[i] = array[i];
+//        }
+//        resArray[index] = element;
+//        for (int i = index + 1; i <= array.length; i++) {
+//            resArray[i] = array[i - 1];
+//        }
+//    }
 
+//    public void add(int index, Object element) {
+//        if (realSize == array.length) {
+//            Object[] resArray = new Object[array.length + 1];
+//            int count = 0;
+//            for (int i = 0; i < resArray.length - 1; i++) {
+//                if (i == index) {
+//                    resArray[i] = element;
+//                } else {
+//                    resArray[i] = array[count];
+//                    count++;
+//                }
+//            }
+//        }
+//        realSize++;
+//    }
+
+    public void add(int index, Object element) {
+        Object[] resArray = new Object[array.length + 1];
+        for (int i = 0; i < index; i++) {
+            resArray[i] = array[i];
+        }
+        resArray[index] = element;
+        for (int j = index + 1; j < resArray.length; j++) {
+            resArray[j] = array[j - 1];
+        }
+        array = resArray;
+        realSize++;
     }
 
     public Object remove(int index) {
-        return null;
+        checkIndex(index);
+        Object resElement = array[index];
+
+//        for (int i = 0; i < array.length - 1; i++) {
+//            array[i] = array[i + 1];
+//        }
+
+        if (array.length - 1 - index >= 0) {
+            System.arraycopy(array, index + 1, array, index, array.length - 1 - index);
+        }
+        realSize--;
+        return resElement;
+    }
+
+    public void checkIndex(int index) {
+        if (!isCorrectIndex(index)) {
+            throw new ArrayIndexOutOfBoundsException("Incorrect index");
+        }
+    }
+
+    private boolean isCorrectIndex(int index) {
+        if (index > -1 || index < realSize) {
+            return true;
+        }
+        return false;
     }
 
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == o) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public int lastIndexOf(Object o) {
-        return 0;
+        int lastIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == o) {
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
     }
 
     @Override
--- src/main/java/ru/itsjava/collections/lists/arrayList/MyArrayListPractice.java
+++ src/main/java/ru/itsjava/collections/lists/arrayList/MyArrayListPractice.java
@@ -7,32 +7,57 @@ public class MyArrayListPractice {
 
         System.out.println("list.size() = " + list.size());
         list.add("Hello");
-        list.add("Bye");
+        list.add("Bye1");
         System.out.println("list.size() = " + list.size());
 
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
-        list.add("Bye");
+        list.add("Bye2");
+        list.add("Bye3");
+        list.add("Bye4");
+        list.add("Bye5");
+        list.add("Bye6");
+        list.add("Bye7");
+        list.add("Bye8");
+        list.add("Bye8");
+        list.add("Bye8");
         System.out.println("list.size() = " + list.size());
         System.out.println("list.toString() = " + list);
 
-        MyArrayList list2 = new MyArrayList();
+//        MyArrayList list2 = new MyArrayList();
+//
+//        System.out.println("list2.isEmpty() = " + list2.isEmpty());
+//        list2.add("Hello");
+//        System.out.println("list2.isEmpty() = " + list2.isEmpty());
+//
+//        System.out.println("list.contains(\"Daniil\") = " + list.contains("Daniil"));
+//        System.out.println("list.contains(\"Hello\") = " + list.contains("Hello"));
 
-        System.out.println("list2.isEmpty() = " + list2.isEmpty());
-        list2.add("Hello");
-        System.out.println("list2.isEmpty() = " + list2.isEmpty());
+//        list.clear();
+//        System.out.println("list = " + list);
 
-        System.out.println("list.contains(\"Daniil\") = " + list.contains("Daniil"));
-        System.out.println("list.contains(\"Hello\") = " + list.contains("Hello"));
+//        System.out.println("list.remove(\"Bye2\") = " + list.remove("Bye2"));
+//        System.out.println("list.size() = " + list.size());
+//        System.out.println("list = " + list);
 
-        list.clear();
+        System.out.println("list.remove(2) = " + list.remove(2));
         System.out.println("list = " + list);
+        System.out.println("list.remove(\"Bye7\") = " + list.remove("Bye7"));
+        System.out.println("list = " + list);
+
+        System.out.println("list.indexOf(\"Hello\") = " + list.indexOf("Hello"));
+        System.out.println("list.indexOf(\"Bye4\") = " + list.indexOf("Bye4"));
+
+        System.out.println("list.lastIndexOf(\"Bye8\") = " + list.lastIndexOf("Bye8"));
+
+        System.out.println("list.get(4) = " + list.get(4));
+        System.out.println("list.get(0) = " + list.get(0));
+
+        System.out.println("list.set(7,\"Hello_again\") = " + list.set(7, "Hello_again"));
+        System.out.println("list = " + list);
+
+        System.out.println("list.size() = " + list.size());
+        list.add(4, "Goodbye");
+        System.out.println("list = " + list);
+        System.out.println("list.size() = " + list.size());
     }
 
 }
