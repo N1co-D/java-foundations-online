19200e96b82d255cbb89a1fbd5f17ba0fa0a12f6
dmark <dmarkevich@gmail.com>
2023-02-09 20:43:57 +0300

    Creatable (HW)

--- src/main/java/ru/itsjava/interfaces/Creatable.java
+++ src/main/java/ru/itsjava/interfaces/Creatable.java
@@ -1,10 +1,9 @@
 package ru.itsjava.interfaces;
 
 public interface Creatable {
-
     void createWish();
 
-    default void canYouCreateWish(){
+    default void canYouCreateWish() {
         System.out.println("Я могу исполнить твоё желание!");
     }
 
